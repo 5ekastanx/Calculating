f2e2013eef6c61559a1a430c6c76f01a266b599c
Bekastan <5ekastan@example.com>
2024-08-19 19:30:27 +0300

    React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculating1 from './calculating1/calculating1'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Calculating1/>
+    </div>
+  )
+}
+
+export default App
